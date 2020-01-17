31d6db39ecd4303f3a73bf8a37669eaf12fae025
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-17 18:27:21 +0500

    functions
    Simple working of fuctions is observed in this code.

---
+++ functions.js
@@ -0,0 +1,15 @@
+function student(fname,lname,rollno,libraryno)
+{
+
+   var result = "";
+
+   result += "This student "+fname+" "+lname+" having roll no "+rollno+" with library no "+libraryno;
+
+    return result;
+
+}
+
+var a=student("Ahmed","Ali",1234,5454)
+console.log(a);
+
+
