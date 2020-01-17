02aba5f893a0f1d495624964eb2b5031d84328c2
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-17 20:18:05 +0500

    2Darray
    explored the two dimentional array and the push to an array of name has been placed.

---
+++ twodimentionalandpush.js
@@ -0,0 +1,11 @@
+var myarray = [[1,2,4,5],[23,34,55,56],[123,344,4,445,334],[34,45,31,14,5]];
+
+var mydata = myarray[1][2];
+
+console.log(mydata);
+
+var namearray = ["WestIndies","Game","Odi","Captain"];
+
+namearray.push(["WorldT20"],["twice"]);
+
+console.log(namearray);
