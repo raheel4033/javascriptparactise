8b26ad53d49a2f23530114ebcaa053b70616e481
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-17 20:46:29 +0500

    calculator
    simple calculator with all the function applications.

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(a,b)
+{
+    console.log(a+b);
+}
+function subtract(a,b)
+{
+    console.log(a-b);
+}
+function multiplication(a,b)
+{
+    console.log(a*b);
+}
+function divide(a,b)
+{
+    console.log(a-b);
+}
+add(10,3);
+subtract(18,4);
+multiplication(4,4);
+divide(12,3);
