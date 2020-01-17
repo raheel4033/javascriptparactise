653c18a9b3ec2066975ab01ccf8088ff687148aa
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-17 22:31:45 +0500

    conditionals
    I've explored the conditionals with the &&operators and the code where both the conditions must be true.

---
+++ conditionals.js
@@ -0,0 +1,18 @@
+var name = "Raheel";
+var password = 1234;
+
+if(name == "Raheel" && password == 1234)
+{
+       console.log("You are allowed to enter");
+        console.log(1+1);
+}
+else
+{
+    console.log("Invalid Login Bro!");
+    console.log(2+2);
+}
+
+
+
+
+
