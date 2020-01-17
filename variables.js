6a6c12772cad032134f5a5c6f5d708bffce72438
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-17 17:34:54 +0500

    variables
    the variables are declared and different values are observed in the variables.

---
+++ variables.js
@@ -0,0 +1,23 @@
+var name = "Raheel";
+
+console.log(name);
+
+let game = "Gta Vice-City"
+
+console.log(game);
+
+var number = 8;
+
+console.log(number);
+
+var a = 5;
+
+
+a = a+5;
+
+console.log(a);
+
+
+const pi = 3.142; // the const pi cann't be changed or altered as a = 5
+
+console.log(pi);
