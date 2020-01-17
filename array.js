1d95e38194e294c707efa12387648d2d2d2a61f2
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-17 20:07:59 +0500

    arraycreation
    Basic array in JS

---
+++ array.js
@@ -0,0 +1,13 @@
+var myarray = [1,2,3,4,5,6,7]
+
+
+var mydata = myarray[3];
+
+
+console.log(mydata);
+
+
+var namearray = ["Hammad","Ahmed","Bilal","Armaghan","Umer"];
+
+
+console.log(namearray[1]);
