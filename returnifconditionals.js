9e9173ca89108ce4f29eacc8bc971f697997e6ec
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-19 11:23:28 +0500

    returnconditions
    conditionals with if else in javascript

---
+++ returnifconditionals.js
@@ -0,0 +1,15 @@
+function myname(val)
+{
+    if(val==10)
+    {
+        return("Correct Value");
+    }
+    else
+    {
+        return("Return False");
+    }
+}
+
+
+console.log(myname(10));
+console.log(myname(7));
