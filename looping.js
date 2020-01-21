7104335eed16c91dd1bf00e8bcc430368c2de221
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-21 21:56:48 +0500

    forloop
    looping practise code in js,

---
+++ looping.js
@@ -0,0 +1,12 @@
+var todo = [
+      "Take classes",
+      "Get a job",
+      "Practise programming"
+];
+
+
+for(var i=0;i<todo.length;i++)
+{
+    todo[i]=todo[i]+"!";
+    console.log(todo[i]);
+}
