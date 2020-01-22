1a9eee53c5a4e24119b76c813565a0fd60705f44
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-23 00:29:05 +0500

    nestedarray.js
    The list and nested array with the list.

---
+++ nestedarrays.js
@@ -0,0 +1,25 @@
+var myPlants = 
+[
+    {
+    type:"flowers",
+    list:[
+        "rose",
+        "tulip",
+        "Dandelion"
+    ]
+},
+{
+    type:"trees",
+    list:[
+        "fir",
+        "pine",
+        "birch"
+    ]
+}
+
+];
+
+var firstFlowers = myPlants[0].list;
+console.log(firstFlowers);
+var secondTree =myPlants[1].list;
+console.log(secondTree);
