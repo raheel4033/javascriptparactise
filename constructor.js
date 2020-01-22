0f3fcb59a27d490be257d4143e3bf1002d168485
raheel4033 <48746709+raheel4033@users.noreply.github.com>
2020-01-22 19:39:45 +0500

    constructor
    constructor function has been implemented.

---
+++ constructor.js
@@ -0,0 +1,10 @@
+function user(firstname,lastname,age,salary)
+{
+    this.firstname =firstname;
+    this.lastname = lastname;
+    this.age= age;
+    this.salary = salary;
+}
+
+var user1 =new user('John','Smith',24,10000);
+console.log(user1);
